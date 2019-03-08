56d89b33eb847f7a213a2d74a4757726f4624fc6
Rachel Chen <rachelchen0418@gmail.com>
2019-03-08 17:27:54 -0500

    Initial Commit

---
+++ MarioKartGestures2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKartGestures2
+//
+//  Created by Rachel Chen on 3/8/19.
+//  Copyright © 2019 rachelchen0418. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
